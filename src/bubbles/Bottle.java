7b1ed9afe1a84c4822c40f61b6fd9eb72257e43b
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-13 14:54:16 +0200

    Added: bubbetask

---
+++ src/bubbles/Bottle.java
@@ -0,0 +1,6 @@
+package bubbles;
+
+public class Bottle {
+    int volume;
+    int water;
+}
---
+++ src/bubbles/Water.java
@@ -0,0 +1,14 @@
+package bubbles;
+
+public abstract class Water {
+    public int color;
+    public int transparency;
+    public int smell;
+    public int temperature;
+
+
+    public void pump(Bubble[] bubbles) {
+
+
+    }
+}
