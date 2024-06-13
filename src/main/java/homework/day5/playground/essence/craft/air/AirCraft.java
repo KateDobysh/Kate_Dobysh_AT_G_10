478fe27c0b4a90adf514a15d7795991f807f9330
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-13 20:47:34 +0200

    playground_essence_rideable

--- src/main/java/homework/day5/playground/essence/craft/air/AirCraft.java
+++ src/main/java/homework/day5/playground/essence/craft/air/AirCraft.java
@@ -2,8 +2,9 @@ package homework.day5.playground.essence.craft.air;
 
 import homework.day5.playground.essence.Flyable;
 import homework.day5.playground.essence.Matter;
+import homework.day5.playground.essence.craft.Transportable;
 
-public abstract class AirCraft extends Matter implements Flyable {
+public abstract class AirCraft extends Matter implements Flyable, Transportable {
     protected static String name;
 
     public AirCraft(int mass, String name) {
