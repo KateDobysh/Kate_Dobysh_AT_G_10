82a222d3884b6c4126b0942b9c119019532183ed
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-15 09:50:30 +0200

    hometask_day10_task5

---
+++ src/main/java/homework/day6/stringTask/ColumnLetters.java
@@ -0,0 +1,11 @@
+package homework.day6.stringTask;
+
+public class ColumnLetters {
+    public static void main(String[] args){
+        String text = "I can sit and look at it for hours.";
+        char[] lettersInColumn = text.toCharArray();
+        for (char letter : lettersInColumn){
+            System.out.println(letter);
+        }
+    }
+}
