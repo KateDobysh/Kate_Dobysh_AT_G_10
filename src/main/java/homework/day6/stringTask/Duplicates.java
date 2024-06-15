dd353ee480278fd1e17f98e241b07d761929d465
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-15 23:42:30 +0200

    hometask_StringTask_duplicates

--- src/main/java/homework/day6/stringTask/Duplicates.java
+++ src/main/java/homework/day6/stringTask/Duplicates.java
@@ -1,14 +1,20 @@
 package homework.day6.stringTask;
 
+import java.util.Arrays;
+
 public class Duplicates {
     public static void main(String[] args) {
-//        String text = "I can sit and I can look at it for hours.";
-//        String[] wordDuplicates = text.split(" ");
-//        for (String word : wordDuplicates) {
-//            if () {
-//                System.out.println(word);
-//            }
-//        }
-//    }
+        String text = "I can sit and I can look";
+        String[] wordDuplicates = text.split(" ");
+        String[] temporary = new String[wordDuplicates.length];
+
+
+        for (String i = 0; i < wordDuplicates.length; i++ ){
+            String word = wordDuplicates[i];
+            if (wordDuplicates[i].equals(i)){
+                temporary[0] = i;
+                System.out.println(Arrays.toString(wordDuplicates));
+            }
+        }
     }
 }
