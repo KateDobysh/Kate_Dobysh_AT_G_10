3923b5d4b7d7fddcdea535e9839025874970e260
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-18 18:10:28 +0200

    hometask_stringtask

--- src/main/java/homework/day6/stringTask/CurrentDateTime.java
+++ src/main/java/homework/day6/stringTask/CurrentDateTime.java
@@ -1,14 +1,30 @@
 package homework.day6.stringTask;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class CurrentDateTime {
-    public static void main(String[] args){
-        LocalTime now = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        String dateTimeString = now.format(formatter);
-        System.out.println(dateTimeString);
+    public static void main(String[] args) {
+
+
+//        Date currentDate = new Date();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("Day Month year, HH hours mm minutes");
+//        String currentDateTime = dateFormat.format(currentDate);
 
     }
 }
+
+
+
+
+
+
+
+
+
+//        LocalTime now = LocalTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+//        String dateTimeString = now.format(formatter);
+//        System.out.println(dateTimeString);
