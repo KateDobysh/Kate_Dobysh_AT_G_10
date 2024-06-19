1491f181f4cb341990a413c3e9a2ae158a11f0dd
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-19 23:59:17 +0200

    hometask_duplicates

--- src/main/java/homework/day6/stringTask/Duplicates.java
+++ src/main/java/homework/day6/stringTask/Duplicates.java
@@ -6,9 +6,14 @@ public class Duplicates {
     public static void main(String[] args) {
         String text = "I can sit and I can look";
         String[] textSplitted = text.split(" ");
-        String[] temporary = new String[textSplitted.length];
-
-
-
+        String duplicates = "";
+        for (int i = 0; i < textSplitted.length; i++) {
+            for (int j = i + 1; j < textSplitted.length; j++) {
+                if (textSplitted[i].equals(textSplitted[j]) && !duplicates.contains(textSplitted[i])) {
+                    duplicates += textSplitted[i] + " ";
+                    break;
+                }
+            }
+        }
     }
 }
