cd40835246cb42e1c286fd92daad95c27a7cefda
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-20 00:23:21 +0200

    hometask_stringTaskRestructured

---
+++ src/main/java/homework/day7/stringTask/CurrentDateTime.java
@@ -0,0 +1,18 @@
+package homework.day7.stringTask;
+
+public class CurrentDateTime {
+    public static void main(String[] args) {
+
+
+//        Date currentDate = new Date();
+//        SimpleDateFormat dateFormat = new SimpleDateFormat("Day Month year, HH hours mm minutes");
+//        String currentDateTime = dateFormat.format(currentDate);
+
+    }
+}
+
+
+//        LocalTime now = LocalTime.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+//        String dateTimeString = now.format(formatter);
+//        System.out.println(dateTimeString);
---
+++ src/main/java/homework/day7/stringTask/Duplicates.java
@@ -0,0 +1,17 @@
+package homework.day7.stringTask;
+public class Duplicates {
+
+    public static void main(String[] args) {
+        String text = "I can sit and I can look";
+        String[] textSplitted = text.split(" ");
+        String duplicates = "";
+        for (int i = 0; i < textSplitted.length; i++) {
+            for (int j = i + 1; j < textSplitted.length; j++) {
+                if (textSplitted[i].equals(textSplitted[j]) && !duplicates.contains(textSplitted[i])) {
+                    duplicates += textSplitted[i] + " ";
+                    break;
+                }
+            }
+        }
+    }
+}
