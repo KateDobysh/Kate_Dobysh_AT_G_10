15d54c512732f59c753371b423f1728e29020e87
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-23 23:28:27 +0200

    homework_Added_readEditNewInput

--- src/main/java/homework_day8/ReadEditNewInput.java
+++ src/main/java/homework_day8/ReadEditNewInput.java
@@ -1,22 +1,45 @@
 package homework_day8;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
+import java.io.*;
 
 public class ReadEditNewInput {
     public static void main(String[] args) {
+        String filePut = null;
         try {
             FileInputStream fis = new FileInputStream("serFile.txt");
             ObjectInputStream ois = new ObjectInputStream(fis);
-            String filePut = (String) ois.readObject();
+            filePut = (String) ois.readObject();
             System.out.println("Your file was deserialized ");
         } catch (IOException | ClassNotFoundException e) {
             System.out.println("File not found");
             e.printStackTrace();
         }
+        char[] filePutEdit = filePut.toCharArray();
+        char[] vowelsOnly = new char[filePutEdit.length];
+        int vowels = 0;
+        for (char character : filePutEdit) {
+            if (isVowel(character)) {
+                vowelsOnly[vowels++] = character;
+            }
+        }
     }
-    public void editFile(){
 
+    private static boolean isVowel(char character) {
+        return true;
+    }
+
+    {
+        FileOutputStream fis = null;
+        try {
+            fis = new FileOutputStream("serFile.txt");
+        } catch (FileNotFoundException ex) {
+            throw new RuntimeException(ex);
+        }
+        ObjectOutputStream oos = null;
+        try {
+            oos = new ObjectOutputStream(fis);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
