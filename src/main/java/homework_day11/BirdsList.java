1403b6d5211bfbbfaa470f5a7e3f2a6aaee894b2
Kate Dobysh <katsiaryna.dobysh@gmail.com>
2024-06-24 20:35:26 +0200

    homework_Lists_Birds

---
+++ src/main/java/homework_day11/BirdsList.java
@@ -0,0 +1,30 @@
+package homework_day11;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class BirdsList {
+    public static void main(String[] args) {
+        List<String> birds = Arrays.asList("Чайка", "Дрозд", "Бусел", "Голубь", "Воробей", "Цапля");
+        for (String bird : birds) {
+            System.out.println("--" + bird + "--");
+        }
+        Set<Character> allVowels = new HashSet<>(Arrays.asList('а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я', 'А', 'Е', 'Ё', 'И', 'О', 'У', 'Ы', 'Э', 'Ю', 'Я'));
+        int countVowels = 0;
+        int countBirds = 0;
+        for (String bird : birds) {
+            for (char letter : bird.toCharArray()) {
+                if (allVowels.contains(letter)) {
+                    countVowels++;
+                }
+            }
+            if (countVowels > 1) {
+                countBirds++;
+            }
+        }
+        System.out.println("Amount of birds that contain more than 1 vowel: " + countBirds);
+    }
+}
